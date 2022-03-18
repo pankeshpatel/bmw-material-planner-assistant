bbebf8880cc32336138140255a95f27887bdd57c
JINU98 <jinendramalekar@gmail.com>
2022-03-18 16:15:08 +0530

    test

--- frontend/src/components/dashboard-sidebar.js
+++ frontend/src/components/dashboard-sidebar.js
@@ -13,7 +13,7 @@ import { User as UserIcon } from '../icons/user';
 import { UserAdd as UserAddIcon } from '../icons/user-add';
 import { Users as UsersIcon } from '../icons/users';
 import { XCircle as XCircleIcon } from '../icons/x-circle';
-import { Logo } from './logo';
+// import { Logo } from './logo';
 import { NavItem } from './nav-item';
 
 const items = [
@@ -54,7 +54,7 @@ const items = [
   },
   // {
   //   href: '/404',
-  //   icon: (<XCircleIcon fontSize="small" />),
+  //   icon:  (<XCircleIcon fontSize="small" />),
   //   title: 'Error'
   // }
 ];
