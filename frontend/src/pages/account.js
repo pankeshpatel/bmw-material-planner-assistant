a87434b734f07b30f4a81c4124115a2bf4bc8182
Pankesh Patel <pankeshlinux@gmail.com>
2022-03-27 18:23:39 -0400

    restructured components

--- frontend/src/pages/account.js
+++ frontend/src/pages/account.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Box, Container, Grid, Typography } from '@mui/material';
-import { AccountProfile } from '../components/account/account-profile';
+import { AccountProfile } from '../components/account-profile';
 import { DashboardLayout } from '../components/dashboard-layout';
 import { SettingsPassword } from '../components/settings-password';
 
