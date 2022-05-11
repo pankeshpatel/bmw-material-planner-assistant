68220c01dc85f35661649f20caaf25876a9ab003
Pankesh Patel <pankeshlinux@gmail.com>
2022-05-11 02:06:52 -0400

    made some changes in the connection string to mysql

--- backend/config/db.py
+++ backend/config/db.py
@@ -4,7 +4,7 @@ from config.env import settings
 
 
 
-connect_string = f'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(settings.DB_USER, settings.DB_PASSWORD, settings.DB_HOST, settings.DB_PORT, settings.DATABASE)
+connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(settings.DB_USER, settings.DB_PASSWORD, settings.DB_HOST, settings.DB_PORT, settings.DATABASE)
 
 
 
