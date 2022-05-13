e35ec3e10f87fd5c9b8f96a162bd4c8d4b7d2f8d
Pankesh Patel <pankeshlinux@gmail.com>
2022-05-13 01:18:14 -0400

    added dataset  & revised .env variables

--- backend/config/env.py
+++ backend/config/env.py
@@ -2,11 +2,6 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-   DB_USER : str
-   DB_PASSWORD : str 
-   DB_HOST : str 
-   DB_PORT : str
-   DATABASE : str 
    SECRET_KEY : str 
    ALGORITHM : str 
    ACCESS_TOKEN_EXPIRE_DAYS : int 
