33d4d19c92e63593637f3fbc30bc727ff0c663e5
Pankesh Patel <pankeshlinux@gmail.com>
2022-05-23 22:21:57 -0400

    added profiler

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,4 @@
-const api_url = `http://18.208.171.25`
-export const apiUrl = api_url
+//const api_url = `http://18.208.171.25`
+const api_url = `http://localhost:8000`;
+
+export const apiUrl = api_url;
