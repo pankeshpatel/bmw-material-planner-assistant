41bfa00e7a911263df230ccf629a23cd11afec09
Pankesh Patel <pankeshlinux@gmail.com>
2022-05-26 10:26:02 -0400

    improved algorithm

--- backend/routes/healthscore.py
+++ backend/routes/healthscore.py
@@ -36,11 +36,7 @@ list_qty_instance = []  # This is a global
 
 # This function constructs an individual instances of total Quantity fields
 def find_total_quantity_instances(formatted_date: str, material_id: str, safety_stock: int, data:pd.DataFrame()):
-    
-    # sql = """SELECT material, demand_date, total_quantity FROM admin.MD04 WHERE material = %s AND demand_date = %s"""
-    # data= pd.DataFrame(conn.execute(sql, material_id, formatted_date).fetchall(), columns=["material", "demand_date", "total_quantity"])
-    
-    
+        
     item=0
     local_list_qty_instance = []
     global list_qty_instance
@@ -71,10 +67,6 @@ def find_total_quantity_instances(formatted_date: str, material_id: str, safety_
 # This function  constructs a summary (avg, min, max) dataframe total Quantity
 def find_total_quantity_summary(formatted_date: str, material_id: str, safety_stock: int, data:pd.DataFrame()):
     
-    # sql = """SELECT material, demand_date, total_quantity FROM admin.MD04 WHERE material = %s AND demand_date = %s"""
-    # data= pd.DataFrame(conn.execute(sql, material_id, formatted_date).fetchall(), columns=["material", "demand_date", "total_quantity"])
-    
-    #print(data)
     
     # max value
     if(len(data["total_quantity"]) == 0):
@@ -102,8 +94,8 @@ def find_total_quantity_summary(formatted_date: str, material_id: str, safety_st
 # This function is to find stock
 def find_stock(date: str, formatted_date: str, material_id: str, data:pd.DataFrame()) -> int:
             
-    # sql = """SELECT mrp_element, total_quantity, demand_date  FROM admin.MD04 WHERE material = %s AND demand_date = %s""" 
-    # data = pd.DataFrame(conn.execute(sql, material_id, formatted_date).fetchall())
+    #sql = """SELECT mrp_element, total_quantity, demand_date  FROM admin.MD04 WHERE material = %s AND demand_date = %s""" 
+    #data = pd.DataFrame(conn.execute(sql, material_id, formatted_date).fetchall())
     
     # Data is not available in DB
     if(len(data) == 0):
@@ -124,16 +116,16 @@ def find_stock(date: str, formatted_date: str, material_id: str, data:pd.DataFra
             return row["total_quantity"]
 
 
-def find_saftey_stock(date: datetime, data: pd.DataFrame(), saftey_stock: int) -> int:
+def find_saftey_stock(data: pd.DataFrame(), saftey_stock: int) -> int:
     
     for index, row in data.iterrows():   
         safety_stock_qty = abs(row["change_quantity"])    
-        return abs(row["change_quantity"])  # Change due to removal of saftey stock, assumed const
+        return abs(row["change_quantity"])  
 
     # Else
     safety_stock_qty = abs(saftey_stock) 
 
-    return abs(saftey_stock)  # Return last known value of saftey stock
+    return abs(saftey_stock)  
 
 
 
@@ -141,34 +133,6 @@ def format_date(date: datetime) -> str:
     return datetime.date.strftime(date, "%x")
 
 
-def calc_avg_stock_change(data: pd.DataFrame()) -> float:
-    data_filtered = data[data[3] == "Stock"]  # Stock values
-    #print("****data_filtered****")
-    #print(data_filtered)
-    #Assume data is sorted for now
-    prev_row: int
-    curr_row: int
-    counter: int = 1
-    total_dif: int = 0
-    first_val_in = False
-    for index, row in data_filtered.iterrows():
-        if row[5] == "total_quantity":
-            continue
-        elif first_val_in == False:
-            prev_row = int(row[5])
-            first_val_in = True
-        else:
-            curr_row = int(row[5])
-            diff = curr_row - prev_row
-            
-            if diff < 0:
-                total_dif += diff
-                counter += 1
-                prev_row = curr_row
-            else:
-                prev_row = curr_row
-
-    return total_dif/counter
 
 # What is sigmoid function - https://www.youtube.com/watch?v=LcHYy-OZHp8
 def get_health_score(stock: int, saftey_stock: int, k_val: float) -> float:
@@ -184,46 +148,29 @@ def sigmoid(SS: int, k: float) -> float:
     return (((1/(1+math.exp(-k*SS)))-(1/2))*2)*100
 
 
-def print_values(health: float, stock: int, avg_stock_change: float, material: str, formatted_date: str):
-    if stock != None:
-        DoS = stock/avg_stock_change
-        print("Health score for", material, "on",
-              formatted_date, "is", health, "DoS is", DoS)
+
 
 
 @healthscore.get('/{planner_id}/{material_id}', status_code = status.HTTP_200_OK)
 async def get_material_healthscore(planner_id:str, material_id: str, healthdate: str, user_id: int = Depends(get_current_user)):
 
     my_profiler.start("health-score")
-    
-    #material = material_id
     date = healthdate
     num_days = 10  
     
     
-    #sql = """SELECT material  FROM admin.MD04 WHERE material = %s AND demand_date = %s AND planner = %s""" 
-    #data = pd.DataFrame(conn.execute(sql, material_id, healthdate, planner_id).fetchall())
-    
-    
-    # if len(data.columns) == 0:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Data item does not exist")
-    
-    mm, dd, yyyy = map(int, date.split('/'))
-    healthdate = datetime.datetime(yyyy, mm, dd)
-
     sql = """SELECT mrp_element, change_quantity FROM admin.MD04 where material = %s AND planner=%s"""
-    data_safety_stock = pd.DataFrame(conn.execute(sql, material_id, planner_id).fetchall())  
+    data_safety_stock = pd.DataFrame(conn.execute(sql, material_id, planner_id).fetchall())
+    
     
     if len(data_safety_stock.columns) == 0:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Data item does not exist")
 
 
     saftey_stock = 0 # default value 
-    # saftey_stock = find_saftey_stock(format_date(healthdate), data_safety_stock[data_safety_stock["mrp_element"] == "SafeSt"], saftey_stock)
-    saftey_stock = find_saftey_stock(healthdate, data_safety_stock[data_safety_stock["mrp_element"] == "SafeSt"], saftey_stock)
+    
+    saftey_stock = find_saftey_stock(data_safety_stock[data_safety_stock["mrp_element"] == "SafeSt"], saftey_stock)
 
-
-    #avg_stock_change: float = calc_avg_stock_change(data)
 
     mm, dd, yyyy = map(int, date.split('/'))
     date_obj = datetime.datetime(yyyy, mm, dd)
@@ -241,19 +188,13 @@ async def get_material_healthscore(planner_id:str, material_id: str, healthdate:
         data= pd.DataFrame(conn.execute(sql, material_id, formatted_date).fetchall(), columns=["material", "mrp_element", "total_quantity", "demand_date" ])
         
         
-        # # ASSUME DATA SORTED ALREADY
-        #stock = find_stock(format_date(new_date), formatted_date, material_id, data)
         stock = find_stock(new_date, formatted_date, material_id, data)
 
-        
-
-        # Total Quantity
         find_total_quantity_summary(formatted_date, material_id, saftey_stock, data) 
-        
-        # to find each instances of total quantity instances
+
         find_total_quantity_instances(formatted_date, material_id, saftey_stock, data)  
-        
-        
+
+
         health = get_health_score(stock, saftey_stock, k_val=0.8)   
         
         if health != None:
@@ -261,27 +202,23 @@ async def get_material_healthscore(planner_id:str, material_id: str, healthdate:
 
 
     df_total_qty = pd.DataFrame(list_qty, columns = ['material', 'demand_date', 'max', 'min', 'mean', 'safety stock']) 
-    #print(tabulate(df_total_qty, headers = 'keys', tablefmt = 'psql'))
+    print(tabulate(df_total_qty, headers = 'keys', tablefmt = 'psql'))
     
     
     # This would prepare .csv file that contains total_qty_instances
     df_total_qty_instances = pd.DataFrame(list_qty_instance, columns = ['material', 'demand_date', 'total_quantity', 'safety stock']) 
-    #print(tabulate(df_total_qty_instances, headers = 'keys', tablefmt = 'psql'))
+    print(tabulate(df_total_qty_instances, headers = 'keys', tablefmt = 'psql'))
     
     # destruct this global variable
     list_qty_instance.clear()
     list_qty.clear()
     
-    
     result = sum(avg)/len(avg)
     result = round(result, 2)
     percentage_result = str(result).__add__(' %')
     
-    # Retrieve material information
-    
     sql = """SELECT DISTINCT material, material_9, material_7, mat_description, mat_description_eng FROM admin.MaterialMaster where material = %s"""
     df_material = pd.DataFrame(conn.execute(sql, material_id).fetchall(), columns=["material", "material_9" , "material_7", "mat_description", "mat_description_eng"])
-    
 
 
     health_score = {
@@ -296,4 +233,6 @@ async def get_material_healthscore(planner_id:str, material_id: str, healthdate:
     my_profiler.end("health-score")
     my_profiler.log("print")
 
+
+    
     return health_score
--- frontend/src/utils/apihelper.js
+++ frontend/src/utils/apihelper.js
@@ -1,179 +1,173 @@
-import axios from 'axios'
-import {apiUrl, plannerId} from './constants'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { apiUrl, plannerId } from "./constants";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 let token = "";
-let headers= "";
+let headers = "";
 
-const getHeader = ()=>{
-    if (typeof window !== "undefined" && localStorage.getItem("token")) {    
-       token = localStorage.getItem("token");
-        // console.log("token",token)
-        return headers = {
-            headers: {
-                Authorization: `${token ?`Bearer ${token}` : ""}`,
-                // "Content-Type": "multipart/form-data",
-            },
-        };
-    }
-}
+const getHeader = () => {
+  if (typeof window !== "undefined" && localStorage.getItem("token")) {
+    token = localStorage.getItem("token");
+    // console.log("token",token)
+    return (headers = {
+      headers: {
+        Authorization: `${token ? `Bearer ${token}` : ""}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
+};
 
-const notify = (message , status) =>{
-    if(status == "error"){
-        toast.error(message)
-    }
-    else if(status == "success"){
-        toast.success(message)
-    }else if( status =="info"){
-        toast.info(message)
-    }else if( status =="warn"){
-        toast.warn(message)
-    }else {
-        toast(message)
-    }
+const notify = (message, status) => {
+  if (status == "error") {
+    toast.error(message);
+  } else if (status == "success") {
+    toast.success(message);
+  } else if (status == "info") {
+    toast.info(message);
+  } else if (status == "warn") {
+    toast.warn(message);
+  } else {
+    toast(message);
+  }
+};
 
-}
+const handleError = (err) => {
+  console.log("erros", err);
 
+  if (err.response.status == 401) {
+    //
+    notify(JSON.stringify(err.response.data.detail), "error");
+  }
+  if (err.response.status == 400) {
+    notify(JSON.stringify(err.response.data.detail), "error"); //bad request
+  }
+  if (err.response.status == 403) {
+    notify(JSON.stringify(err.response.data.detail), "error"); //bad request
+  }
+  if (err.response.status == 404) {
+    notify(JSON.stringify(err.response.data.detail), "error"); //page not found
+  }
+  if (err.response.status == 408) {
+    //Request timeout -
+    notify(JSON.stringify(err.response.data.detail), "error");
+  }
+  if (err.response.status == 500) {
+    //internal sever
+    notify(JSON.stringify(err.response.data.detail), "error");
+  }
+  if (err.response.status == 502) {
+    //bad gateway - invalid response
+    notify(JSON.stringify(err.response.data.detail), "error");
+  }
+};
 
-const handleError = (err)=>{
-    console.log("erros",err)
-
-     if(err.response.status == 401){ // 
-         notify(JSON.stringify(err.response.data.detail),"error")
-     }
-     if(err.response.status == 400){
-         notify(JSON.stringify(err.response.data.detail),"error") //bad request 
-     }
-     if(err.response.status == 403){
-        notify(JSON.stringify(err.response.data.detail),"error") //bad request 
-    }
-     if(err.response.status == 404){
-         notify(JSON.stringify(err.response.data.detail),"error") //page not found
-     }
-     if(err.response.status == 408){ //Request timeout - 
-         notify(JSON.stringify(err.response.data.detail),"error")
-     }
-     if(err.response.status == 500){ //internal sever
-         notify(JSON.stringify(err.response.data.detail),"error")
-     }
-     if(err.response.status == 502){ //bad gateway - invalid response
-         notify(JSON.stringify(err.response.data.detail),"error")
-     }
-
- }
- 
-
-const loginCall = (body) =>{
-    const url = `${apiUrl}/users/login`
-    return new Promise((resolve,reject)=>{
-        const header = getHeader();
-        const url = `${apiUrl}/users/login`
-        console.log("url",url)
-            axios
-                .post(url,body,header)
-                .then((res)=>{
-                    resolve(res)
-                })
-                .catch((err) => {
-                   
-                    reject(handleError(err))
-                })
-                
-            })
-
-    }
-
-const healthScoreCall = (materialId,healthDate) => {
-    const url = `${apiUrl}/healthscore/${eval(plannerId)}/${materialId}?healthdate=${healthDate}`
-    console.log("url",url)
-    // http://localhost:8000/exceptions/healthscore/115/?start_date=02/18/22&end_date=04/04/22
-    return new Promise((resolve,reject)=>{
+const loginCall = (body) => {
+  const url = `${apiUrl}/users/login`;
+  return new Promise((resolve, reject) => {
     const header = getHeader();
-            axios
-                .get(url,header)
-                .then((res)=>{
-                    resolve(res)
-        
-                })
-                .catch((err) => {
-                    reject(handleError(err))
-                })
-                
-            })
-}
+    const url = `${apiUrl}/users/login`;
+    console.log("url", url);
+    axios
+      .post(url, body, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-const ExceptionManagerCall = (startDate,EndDate) => {
-    const url = `${apiUrl}/exceptions/manager/${eval(plannerId)}?start_date=${startDate}&end_date=${EndDate}`
-    // http://localhost:8000/exceptions/manager/115/?start_date=02/18/22&end_date=04/04/22
-    return new Promise((resolve,reject)=>{
+const healthScoreCall = (materialId, healthDate) => {
+  const url = `${apiUrl}/healthscore/${eval(plannerId)}/${materialId}?healthdate=${healthDate}`;
+  console.log("url", url);
+  // http://localhost:8000/exceptions/healthscore/115/?start_date=02/18/22&end_date=04/04/22
+  return new Promise((resolve, reject) => {
     const header = getHeader();
-            axios
-                .get(url,header)
-                .then((res)=>{
-                    resolve(res)
-        
-                })
-                .catch((err) => {
-                    reject(handleError(err))
-                })
-                
-            })
-}
+    axios
+      .get(url, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-const ExceptionMatrixCall = (startDate,EndDate) => {
-    const url = `${apiUrl}/exceptions/matrix/${eval(plannerId)}?start_date=${startDate}&end_date=${EndDate}`
-    // http://localhost:8000/exceptions/manager/115/?start_date=02/18/22&end_date=04/04/22
-    return new Promise((resolve,reject)=>{
+const ExceptionManagerCall = (startDate, EndDate) => {
+  const url = `${apiUrl}/exceptions/manager/${eval(
+    plannerId
+  )}?start_date=${startDate}&end_date=${EndDate}`;
+  // http://localhost:8000/exceptions/manager/115/?start_date=02/18/22&end_date=04/04/22
+  return new Promise((resolve, reject) => {
     const header = getHeader();
-            axios
-                .get(url,header)
-                .then((res)=>{
-                    resolve(res)
-        
-                })
-                .catch((err) => {
-                    reject(handleError(err))
-                })
-                
-            })
-}
+    axios
+      .get(url, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-const plannerIdCall = () =>{
-    const url = `${apiUrl}/planners/planner-id/${eval(plannerId)}`
-    return new Promise((resolve,reject)=>{
+const ExceptionMatrixCall = (startDate, EndDate) => {
+  const url = `${apiUrl}/exceptions/matrix/${eval(
+    plannerId
+  )}?start_date=${startDate}&end_date=${EndDate}`;
+  // http://localhost:8000/exceptions/manager/115/?start_date=02/18/22&end_date=04/04/22
+  return new Promise((resolve, reject) => {
     const header = getHeader();
-            axios
-                .get(url,header)
-                .then((res)=>{
-                    resolve(res)
-        
-                })
-                .catch((err) => {
-                    reject(handleError(err))
-                })
-                
-            })
-    
-}
+    axios
+      .get(url, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-const matetrialCall = ()=>{
-    const url = `${apiUrl}/materials/${eval(plannerId)}`
-    return new Promise((resolve,reject)=>{
+const plannerIdCall = () => {
+  const url = `${apiUrl}/planners/planner-id/${eval(plannerId)}`;
+  return new Promise((resolve, reject) => {
     const header = getHeader();
-            axios
-                .get(url,header)
-                .then((res)=>{
-                    resolve(res)
-        
-                })
-                .catch((err) => {
-                    reject(handleError(err))
-                })
-                
-            })
+    axios
+      .get(url, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-}
+const matetrialCall = () => {
+  const url = `${apiUrl}/materials/${eval(plannerId)}`;
+  return new Promise((resolve, reject) => {
+    const header = getHeader();
+    axios
+      .get(url, header)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(handleError(err));
+      });
+  });
+};
 
-
-export {loginCall,healthScoreCall,ExceptionManagerCall,ExceptionMatrixCall,plannerIdCall,matetrialCall}
+export {
+  loginCall,
+  healthScoreCall,
+  ExceptionManagerCall,
+  ExceptionMatrixCall,
+  plannerIdCall,
+  matetrialCall,
+};
