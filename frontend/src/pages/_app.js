9ca02a022d1b8e633cc036342c3cdd1c9ae28d7f
JINU98 <jinendramalekar@gmail.com>
2022-05-26 20:17:00 +0530

    favicon"

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -25,6 +25,7 @@ const App = (props) => {
         BMW Material Planner
         </title>
         <link rel="icon" type="image/x-icon" href="https://pngimg.com/uploads/bmw_logo/bmw_logo_PNG19714.png"></link>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Roboto:ital,wght@0,300;400;600,1,300&display=swap" rel="stylesheet" /> 
